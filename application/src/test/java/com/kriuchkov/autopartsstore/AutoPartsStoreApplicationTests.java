acc856d94807ad67a054f22237d06a566ce8a660
Dmytro Kriuchkov <kriuchkovdima@gmail.com>
2024-01-14 12:41:09 +0200

    Init project

---
+++ application/src/test/java/com/kriuchkov/autopartsstore/AutoPartsStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kriuchkov.autopartsstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoPartsStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
